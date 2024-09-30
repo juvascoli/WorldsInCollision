b232e1a668185bbb404560dc3633a0194318c5c4
cadenasza <leonardocadena2005@gmail.com>
2024-09-30 08:56:59 -0300

    adicionando a imagem na pagina not found

--- cp04/src/app/error/not-found.tsx
+++ cp04/src/app/error/not-found.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image"
+
 export default function notError() {
     return (
         <div>
             OPS! error 404! Page not Found
+            <figure>
+                <Image src="/image/404-page.jpg"
+                    width={500}
+                    height={500}
+                    alt={"error"} />
+                <figcaption>Erro 404</figcaption>
+            </figure>
         </div>
     )
 }
