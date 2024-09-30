0d45b20e4ba9216e47644dd6c918929e8d27f819
JenniferDaleffi <jenniferdaleffii@gmail.com>
2024-09-30 09:18:14 -0300

    Criacao Menu E cabecalho

--- cp04/src/components/Cabecalho/Cabecalho.tsx
+++ cp04/src/components/Cabecalho/Cabecalho.tsx
@@ -1,9 +1,10 @@
 
-
+import Menu from "../Menu/Menu"
 export default function Cabecalho() {
     return (
         <div>
             <h1>Cabecalho</h1>
+            <Menu />
         </div>
     )
 }
